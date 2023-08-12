bc69e303a617a0935be01d18ae7c5f616eb61d6f
riskiarihidayat <riskiarihdyt@gmail.com>
2023-08-13 00:21:10 +0700

    menambahkan responsive pada navbar dan menambahkan section hero termasuk dengan stylingnya

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let navLinks = document.getElementById('navbar-links');
+let menu = document.getElementById('menu');
+
+menu.addEventListener('click', function () {
+   navLinks.classList.toggle('active');
+});
